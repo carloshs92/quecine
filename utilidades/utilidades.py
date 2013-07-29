07ab9854d26fbf5d6df1b4ae93ddb90ecfd302f8
Carlos Huamani <carlos.hs.92@gmail.com>
2013-07-29 00:57:32 -0500

    ola k aze

--- utilidades/utilidades.py
+++ utilidades/utilidades.py
@@ -24,6 +24,11 @@ def getPeliculas(url, cine):
         # Obtengo Peliculas
         for td in soup.find_all('td', class_="titulo_pelicula"):
             print td.string.encode('utf-8', 'ignore')
+    elif cine == 'cinemark':
+        for a in soup.find_all('a', class_="black"):
+            peli = a.string.encode('utf-8', 'ignore')
+            if peli not in ['Olvidaste tu contraseña?','Regístrate','Inicio']:
+                print peli
 
 
 def getCines(url, cine):
@@ -32,6 +37,9 @@ def getCines(url, cine):
     if cine == 'cineplanet':
         for option in soup.find_all('option'):
             print option.string.encode('utf-8', 'ignore')
+    elif cine == 'cinemark':
+        for h2 in soup.find_all('h2', class_='title2'):
+            print h2.string.encode('utf-8', 'ignore')
 
 
 def getHorarios(url, cine):
@@ -49,7 +57,7 @@ def getHorarios(url, cine):
 
             for a in soup.find_all('a', class_="titulo_pelicula5"):
                 var = a.string.encode('utf-8', 'ignore').strip()
-                if n%2 == 0:
+                if n % 2 == 0:
                     lista.append(var)
                     cartelera[len(cartelera)+1] = lista
                 else:
@@ -57,12 +65,46 @@ def getHorarios(url, cine):
                     lista.append(var)
                 n = n + 1
             print cartelera
+    elif cine == 'cinemark':
+        for loc in soup.find_all('div', class_="item-block-details"):
+            soup = getBeautifulSoup('http://www.cinemark-peru.com%s' % loc.a.attrs['href'])
+            print '######################'
+            print loc.h2.string.encode('utf-8', 'ignore')
+            print '######################'
+            for div in soup.find_all('div', class_="movie-prog2"):
+                print div.h5.next_element.encode('utf-8', 'ignore')
+                print div.h5.span.string
+                print div.li.string
 
-# Defino URL
-#url = "http://www.cineplanet.com.pe/cartelera.php"
 
-#getPeliculas(url, 'cineplanet')
+def testingCineplanet():
+    print "\n\n\n\n#-------CINEPLANET---------#\n\n"
 
-url = "http://www.cineplanet.com.pe/nuestroscines.php"
+    url = "http://www.cineplanet.com.pe/cartelera.php"
 
-getHorarios(url, 'cineplanet')
+    print "\n#-------PELICULAS---------#\n"
+    getPeliculas(url, 'cineplanet')
+
+    url = "http://www.cineplanet.com.pe/nuestroscines.php"
+
+    print "\n#-------CINES---------#\n"
+    getCines(url, 'cineplanet')
+
+    print "\n#-------HORARIOS---------#\n"
+    getHorarios(url, 'cineplanet')
+
+
+def testingCinemark():
+    print "\n\n\n\n#-------CINEMARK---------#\n\n"
+    url = 'http://www.cinemark-peru.com/cartelera'
+
+    print "\n#-------PELICULAS---------#\n"
+    getPeliculas(url, 'cinemark')
+
+    url = 'http://www.cinemark-peru.com/cines'
+
+    print "\n#-------CINES---------#\n"
+    getCines(url, 'cinemark')
+
+    print "\n#-------HORARIOS---------#\n"
+    getHorarios(url, 'cinemark')
