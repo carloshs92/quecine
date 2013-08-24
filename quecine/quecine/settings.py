d1a8eff5f02ce055d379125e734a58a5934ee1e8
Carlos Huamani <carlos.hs.92@gmail.com>
2013-08-24 01:04:11 -0500

    estoy bien

--- quecine/quecine/settings.py
+++ quecine/quecine/settings.py
@@ -18,13 +18,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dco67f12vr4lko',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dco67f12vr4lko',
         'USER': 'pnikxvvnpaabxn',
         'PASSWORD': 'bd3HwhxaMRm5Yv3nJ8cdkIh9g1',
-        'HOST' : 'ec2-54-221-204-45.compute-1.amazonaws.com',
-        'PORT': '5432',                      # Set to empty string for default.
+        'HOST': 'ec2-54-221-204-45.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
